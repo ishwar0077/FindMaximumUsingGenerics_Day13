d80a5b3141ec28178b07895232607865ebbf1dc7
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-10-18 15:49:55 +0530

    Finding Maximum integer Number

--- Program.cs
+++ Program.cs
@@ -6,8 +6,9 @@ namespace FindMaximumUsingGenerics
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Program for Find max.");
             MaximumNumber maximumNumber = new MaximumNumber(); //creating Object
-          int max = maximumNumber.MaximumIntegerNumber(21, 20, 19); 
+          int max = maximumNumber.MaximumIntegerNumber(21, 22, 19); 
             Console.WriteLine("Maximum Numbr is: {0}",max);
 
         }
