0785b6ca50a498408086763e77daa57ee5973b38
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-10-18 19:40:19 +0530

    Findin max of float number3

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace FindMaximumUsingGenerics
             Console.WriteLine("Program for Find max");
             Console.WriteLine("--------------------");
             MaximumNumber maximumNumber = new MaximumNumber(); //creating Object
-          double max = maximumNumber.MaximumIntegerNumber(24.9, 27.1, 23.2); 
+          double max = maximumNumber.MaximumIntegerNumber(24.9, 27.1, 29.2); 
             Console.WriteLine("Maximum Numbr is: {0}",max);
 
         }
