a060b98c747324ce08c720f8cc3ba4c919828ded
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-10-18 15:35:35 +0530

    Finding Maximum Integer Number test case 1

---
+++ MaximumNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumUisngGenerics
+{
+    public class MaximumNumber
+    {
+        //Method for Find Maximum Number
+        public int MaximumIntegerNumber(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            if(firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0)
+            {
+                return firstNumber;
+            }
+            else if(secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0)
+            {
+                return secondNumber;
+            }
+            else if(thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0)
+            {
+                return thirdNumber;
+            }
+            else
+            {
+                
+                return 0;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using FindMaximumUisngGenerics;
+
+namespace FindMaximumUsingGenerics
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MaximumNumber maximumNumber = new MaximumNumber(); //creating Object
+          int max = maximumNumber.MaximumIntegerNumber(21, 20, 19); 
+            Console.WriteLine("Maximum Numbr is: {0}",max);
+
+        }
+    }
+
+}
