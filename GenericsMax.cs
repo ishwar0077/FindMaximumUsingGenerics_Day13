ae6d8ad16b309d8ce16f510ed23b540b4ca62b2f
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-10-19 17:46:34 +0530

    finding max of int, double, string using Generics

---
+++ GenericsMax.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumUisngGenerics
+{
+    public class GenericsMax<T> where T : IComparable
+    {
+         public static void MaxValue(T firstNumber, T secondNumber, T thirdNumber)
+         {
+            if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0)
+            {
+                Console.WriteLine("grater value is: {0}",firstNumber);
+            }
+            else if (secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0)
+            {
+                Console.WriteLine("grater value is: {0}",secondNumber);
+            }
+            else if (thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0)
+            {
+                Console.WriteLine("grater value is: {0}",thirdNumber);
+            }
+           
+         }
+
+
+
+   
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,9 +8,22 @@ namespace FindMaximumUsingGenerics
         {
             Console.WriteLine("Program for Find max");
             Console.WriteLine("--------------------");
-            MaximumNumber maximumNumber = new MaximumNumber(); //creating Object
-          string max = maximumNumber.MaximumIntegerNumber("apple", "ball", "bigBazzar"); 
-            Console.WriteLine("Maximum Numbr is: {0}",max);
+            GenericsMax<int>.MaxValue(25, 22, 21);
+            GenericsMax<int>.MaxValue(22, 30, 25);
+            GenericsMax<int>.MaxValue(55, 23, 45);
+
+            Console.WriteLine("--------------------");
+            GenericsMax<double>.MaxValue(25.9,11.1,22.29);
+            GenericsMax<double>.MaxValue(22.8,77.7,33.3);
+            GenericsMax<double>.MaxValue(45.5,36.1,89.4);
+
+            Console.WriteLine("--------------------");
+            GenericsMax<string>.MaxValue("zink", "Aloe", "ball");
+            GenericsMax<string>.MaxValue("apple", "Rathod", "ball");
+            GenericsMax<string>.MaxValue("apple", "ball", "bigBazzar");
+            Console.WriteLine("--------------------");
+
+
 
         }
     }
